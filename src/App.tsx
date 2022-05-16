df874dc70a82fd07171cada0066419200e8a06eb
Jonathan Jackson <jnthjackson@gmail.com>
2022-05-15 23:09:59 -0400

    New packages. Basic connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,74 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { ethers } from 'ethers'
+import { sequence } from '0xsequence'
+
+
+// Init all the good stuff
+let gIsEthPresent = false;
+const ethereum = (window as any).ethereum; 
+const [walletConnected, setWalletConnected] = useState(false)
+let provider = undefined;
+
+// Check if ya boi is installed
+if (typeof ethereum !== 'undefined') {
+  console.log('MetaMask is installed!');
+  gIsEthPresent = true;
+  provider = new ethers.providers.Web3Provider(ethereum)
+}
+else{
+  console.log('No wallet seem to be installed :/');
+}
+
+async function connectSequenceWallet(){
+  const wallet = new sequence.Wallet('ethereum');
+  const connectDetails = await wallet.connect();
+  console.log('=> connected?', connectDetails.connected);
+  const walletAddress = await wallet.getAddress();
+  console.log(walletAddress);
+  wallet.openWallet();
+}
+
+function connectMetamaskWallet(){
+  if(gIsEthPresent){
+    ethereum.request({ method: 'eth_requestAccounts' }).then()
+  } 
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello loot community</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={ () => connectMetamaskWallet() }>
+            Connect your wallet
           </button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          Follow the socials for updates.
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://twitter.com/FlootFamiliars"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Twitter
           </a>
           {' | '}
           <a
             className="App-link"
-            href="https://vitejs.dev/guide/features.html"
+            href="https://opensea.io/collection/loot-familiars"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Vite Docs
+            OpenSea
           </a>
         </p>
       </header>
